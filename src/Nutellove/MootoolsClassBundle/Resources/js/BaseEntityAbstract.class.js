1c23a3e9a2671c06887ebc655a5b33e099eeadcf
Antoine Goutenoir <antoine.goutenoir@gmail.com>
2011-02-14 13:34:00 +0100

    Abstract Getter and Setter for Properties

--- src/Nutellove/MootoolsClassBundle/Resources/js/BaseEntityAbstract.class.js
+++ src/Nutellove/MootoolsClassBundle/Resources/js/BaseEntityAbstract.class.js
@@ -1,20 +1,23 @@
 /**
  * This is the abstract class for the generated Mootools Base Entity Classes
  * It takes care of the XHR requests for synchronization with the PHP Entities
- * 
+ *
  * @author Antoine Goutenoir <antoine.goutenoir@gmail.com>
  */
 
 var BaseEntityAbstract = new Class({
   Implements: [Options],
   options: {},
-  
+
   entityProperties: {},
   entityMethods: {},
-  
+
+  hasLoaded: false,
+  hasChanged: false,
+
   /**
    * Constructor
-   * 
+   *
    * @param string bundle  The Bundle name
    * @param string entity  The Entity name
    * @param object options The usual options object, defaults above
@@ -25,29 +28,67 @@ var BaseEntityAbstract = new Class({
     this.entity = entity;
     this.setOptions (options);
   },
-  
-  log: function(msg){
+
+  log: function(msg)
+  {
     if (console && console.log) {
-      console.log (msg);
+      console.log (this._getFullEntityName(), msg);
+    }
+
+    return this;
+  },
+
+  error: function(msg)
+  {
+    if (console && console.error) {
+      console.error (this._getFullEntityName(), msg);
+    }
+
+    return this;
+  },
+
+////////////////////////////////////////////////////////////////////////////////
+
+  _getFullEntityName: function()
+  {
+    return this.bundle+'/'+this.entity;
+  },
+
+  _getControllerUrl: function()
+  {
+    // FIXME
+  },
+
+  _getProperty: function(varName)
+  {
+    if (this.entityProperties[varName] != null) {
+      return this.entityProperties[varName];
+    } else {
+      this.log ("Property '"+varName+"' is null.");
+      return null;
     }
   },
 
-  error: function(msg){
-    if (console && console.error) {
-      console.error (msg);
+  _setProperty: function(varName, varValue)
+  {
+    if (this.entityProperties[varName] != null) {
+      if (this.entityProperties[varName] != varValue) {
+        this.entityProperties[varName] = varValue;
+        this.hasChanged = true;
+      } else {
+        this.log ("Setting the same value property '"+varName+"' already has.");
+      }
+    } else {
+      this.log ("Setting a value to the null-defined property '"+varName+"'.");
+      this.entityProperties[varName] = varValue;
+      this.hasChanged = true;
     }
+
+    return this;
   },
-  
-  getFullEntityName: function(){
-    return this.bundle+'/'+this.entity;
-  },
-  
-  getControllerUrl: function(){
-    // FIXME
-  },
-  
+
 ////////////////////////////////////////////////////////////////////////////////
-  
+
   /**
    * Initializes (if needed) the XHR request that'll be used by the save method
    */
@@ -55,16 +96,18 @@ var BaseEntityAbstract = new Class({
     if (!this.saveRequest) {
       var that = this; // better than bind() sometimes
       this.saveRequest = new Request.JSON ({
-        url:       this.getControllerUrl(),
+        url:       this._getControllerUrl(),
         method:    'post',
         onSuccess: function(json, text){
-          that.log ("Save of "+that.getFullEntityName()+" successful.");
+          that.log ("Save of "+that._getFullEntityName()+" successful.");
         },
         onFailure: function(){
-          that.error ("Save of "+that.getFullEntityName()+" failed.");
+          that.error ("Save of "+that._getFullEntityName()+" failed.");
         }
       });
     }
+
+    return this;
   },
 
   /**
@@ -74,17 +117,19 @@ var BaseEntityAbstract = new Class({
     if (!this.loadRequest) {
       var that = this; // better than bind() sometimes
       this.loadRequest = new Request.JSON ({
-        url:       this.getControllerUrl(),
+        url:       this._getControllerUrl(),
         method:    'get',
         onSuccess: function(json, text){
           that.loadJSON (json);
-          that.log ("Load of "+that.getFullEntityName()+" successful.");
+          that.log ("Load of "+that._getFullEntityName()+" successful.");
         },
         onFailure: function(){
-          that.error ("Load of "+that.getFullEntityName()+" failed.");
+          that.error ("Load of "+that._getFullEntityName()+" failed.");
         }
       });
     }
+
+    return this;
   },
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -96,6 +141,7 @@ var BaseEntityAbstract = new Class({
   {
     this.log (json);
     this.entityProperties = json;
+    this.hasLoaded = true;
   },
 
   /**
@@ -110,12 +156,18 @@ var BaseEntityAbstract = new Class({
 
 ////////////////////////////////////////////////////////////////////////////////
 
+  /**
+   * Fetches data from PHP/Database and inject it into the Class
+   */
   load: function()
   {
     this.initializeLoadRequest();
     this.loadRequest.send();
   },
-  
+
+  /**
+   * Sends data to PHP/Database
+   */
   save: function()
   {
     this.initializeSaveRequest();
