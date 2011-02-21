df5571b6a52e84621b37842c4ab42f73a61613ca
Antoine Goutenoir <antoine.goutenoir@gmail.com>
2011-02-21 13:52:29 +0100

    FrontController for generated Controllers

---
+++ src/Nutellove/JavascriptClassBundle/Controller/FrontController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Nutellove\JavascriptClassBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class FrontController extends Controller
+{
+
+  public function loadAction($bundleName, $entityName, $id)
+  {
+    $controllerClass = __NAMESPACE__."\\Entity\\$bundleName\\$entityName"."Controller";
+    if ( ! class_exists($controllerClass) ) {
+      throw new NotFoundHttpException('The targeted Controller does not exist.');
+    }
+
+    $controllerABC = "JavascriptClassBundle:Entity\\$bundleName\\$entityName:load";
+    $response = $this->forward ($controllerABC, array('id' => $id));
+
+    return $response;
+  }
+
+
+
+}
