6c7858e03b8358d03d9a3a54aae0bfb23f5e8f68
AntoineGoutenoir <antoine.goutenoir@gmail.com>
2011-02-25 03:30:13 -0800

    Added methods to CoffeeScript Abstract Class

--- src/Nutellove/JavascriptClassBundle/Resources/public/coffeescript/BaseEntityAbstract.class.cs
+++ src/Nutellove/JavascriptClassBundle/Resources/public/coffeescript/BaseEntityAbstract.class.cs
@@ -3,28 +3,33 @@ class AbstractBaseEntity
   hasLoaded: false
   hasChanged: false
   
-  constructor: (bundle, entity, options)->
-    this.bundle = bundle
-    this.entity = entity
-  log: (msg) -> console?.log? msg ; this
+  constructor: (@bundle, @entity, options) ->
+  log:   (msg) -> console?.log?   msg ; this
+  error: (msg) -> console?.error? msg ; this
+  _getFullEntityName: () -> @bundle + '/' + @entity
+  _getLogPrefix: () ->
+    r = ' - #'+i if i=this.getId?()
+    this._getFullEntityName() + r
   _getProperty: (fieldName) ->
     if this.entityProperties[fieldName]?
       this.entityProperties[fieldName]
     else
-      this.log "Problem" ; null
+      this.log "Field Name "+fieldName+" inexistent." ; null
   _setProperty: (fieldName, fieldValue) ->
     if this.entityProperties[fieldName]?
       this.entityProperties[fieldName] = fieldValue
       this.hasChanged = true : this
     else
-      this.log "Problem" ; null
+      this.log "Field Name "+fieldName+" inexistent." ; this
 
 
 class BaseEntity extends AbstractBaseEntity
   /* FOO VAR */
   foo
-  constructor: (@name) ->
+  #constructor: (@bundle, @entity) ->
 
+  _getBundleName: () -> 'JavascriptClassBundle'
+  _getEntityName: () -> 'Ant'
   getFoo: () -> this._getProperty "foo"
   setFoo: () -> this._setProperty "foo"
 
