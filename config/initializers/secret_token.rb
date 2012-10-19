26db49ac7bd50d77abf14619aa06782813b1fc2a
Goutte <antoine.goutenoir@gmail.com>
2012-10-19 13:45:03 +0200

    New Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cynergiae::Application.config.secret_token = '6233258dd67a4b5a0f1b66ac66c6fa8213a9732eb05b6147a56f6c3cecb304321b4c4cbe268a959000030d4a3a8e14bf70bfe28fab37e65e2bcb40c0ed0a0c59'
